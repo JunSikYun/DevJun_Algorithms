0ef6a1e752ac59ed388278b5873b7f64bb0aa957
JunSik Yoon <98214965+JunSikYun@users.noreply.github.com>
2025-02-05 22:31:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        System.out.println(A+B+C);
+    }
+}
